f996f7ad3e34ca4e5fffacda5950058c21f136f9
sridhar7601 <56919037+sridhar7601@users.noreply.github.com>
2020-10-26 10:23:02 +0530

    Add files via upload

---
+++ rn-starter/App.js
@@ -0,0 +1,19 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import HomeScreen from "./src/screens/HomeScreen";
+import ComponentsScreen from "./src/screens/ComponentsScreen";
+
+const navigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Components : ComponentsScreen
+  },
+  {
+    initialRouteName: "Home",
+    defaultNavigationOptions: {
+      title: "App"
+    }
+  }
+);
+
+export default createAppContainer(navigator);
